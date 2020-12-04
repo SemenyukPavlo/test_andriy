f4020599b73799e04f84f47f6e04772c222fe8f0
pavlosemenyuk <semenyuk.pavlo@gmail.com>
2020-12-04 12:49:25 +0200

    test8

--- test4.js
+++ test4.js
@@ -8,12 +8,5 @@ console.log(11);
 console.log(11);
 console.log(11);
 console.log(11);
-console.log(11);
-console.log(11);
-console.log(11);
-console.log(11);
-console.log(11);
-console.log(11);
-console.log(11);
 console.log(22);
 
